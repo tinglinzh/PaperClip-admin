a58e6fcf67531620777f1e5bb76ae28bf25acd90
tinglinzh <liucheng0416@icloud.com>
2024-11-06 20:31:35 +0800

    feat: fonts file submit

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
+import router from '@/router/index.ts'
 import { StrictMode } from 'react'
 import * as ReactDOMClient from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
 import '@/assets/styles/main.css'
 import '../index.css'
+import '@/assets/fonts/result.css'
 
 const rootElement = document.getElementById('root')!
 const root = ReactDOMClient.createRoot(rootElement)
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,17 @@
+import paperclipIcon from '@/assets/images/paperclip.png'
 import { useEffect } from 'react'
 
 export function Login() {
   useEffect(() => {
-
   }, [])
   return (
-    <>
-      <div className=" size-full">
-        <h1></h1>
+    <div className="flex h-screen items-center justify-center bg-[url('@/assets/images/login-bg.png')] bg-contain bg-center bg-no-repeat">
+      <div className="flex items-center rounded-md bg-white shadow-sm">
+        <img src={paperclipIcon} className="size-16" alt="" />
+        <p className="text-2xl">
+          paperclip 管理系统
+        </p>
       </div>
-    </>
+    </div>
   )
 }
